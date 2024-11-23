ce7b3f0be20773dfa1bb7de5b56042325a55f99c
svemyh <s-myhre@outlook.com>
2024-11-23 08:07:11 -0800

    refactor: change all ports 8090 -> 8000 to ensure consistency

--- backend/main.py
+++ backend/main.py
@@ -243,4 +243,4 @@ async def record(
 
 if __name__ == "__main__":
     logger.info("Starting FastAPI application.")
-    uvicorn.run(app, host="127.0.0.1", port=8090)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
