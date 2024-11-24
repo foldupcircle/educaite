3dbd4d35fc2c5a32fa169cb6e48da84413a598f4
Sricharan Guddanti <sriguddanti@gmail.com>
2024-11-23 16:21:54 -0800

    ckpt, working

--- backend/main.py
+++ backend/main.py
@@ -242,3 +242,4 @@ if __name__ == "__main__":
     logger.info("Starting FastAPI application.")
     # uvicorn.run(app, host="127.0.0.1", port=8000)
     uvicorn.run(app, host="localhost", port=8000)
+    
